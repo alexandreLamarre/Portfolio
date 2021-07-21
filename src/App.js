668f989bad4d53686e1989467cd95f2b391c8ab4
alexandreLamarre <alex7285@gmail.com>
2021-07-21 09:09:13 -0400

    react-three-fiber example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import SceneWrapper from './components/3d/sceneWrapper';
 import RegularPolytope from "./components/3d/torus";
 import MainTitle from "./components/titles";
 
 function App() {
   return (
     <div className="App">
-      <MainTitle/>
-      <RegularPolytope/>
+      {/* <MainTitle/>
+      <RegularPolytope/> */}
+      <SceneWrapper/>
     </div>
   );
 }

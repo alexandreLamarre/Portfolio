9f4d592ad819ffc9046c77ffcafed491a203de2f
alexandreLamarre <alex7285@gmail.com>
2021-07-22 14:54:58 -0400

    basic glsl

--- src/components/3d/shaderIcosahedron/shaderIcosahedron.js
+++ src/components/3d/shaderIcosahedron/shaderIcosahedron.js
@@ -1,37 +1,76 @@
 import React, {Suspense, useRef} from 'react';
-import {Canvas, useFrame, useLoader} from '@react-three/fiber'
+import {Canvas, extend, useFrame, useLoader} from '@react-three/fiber'
 import { OrbitControls} from "@react-three/drei";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-// import {shaderMaterial} from "@react-three/drei"
-// import glsl from "babel-plugins-gl/macro";
+import {shaderMaterial} from "@react-three/drei"
+import glsl from "babel-plugin-glsl/macro";
+import * as THREE from "three";
 
-// const IcosahedronShaderMaterial = shaderMaterial(
+/**
+ * For basic introduction to shaders and using it in react-three-fiber, I recommend 
+ * https://www.youtube.com/watch?v=kxXaIHi1j4w&ab_channel=WrongAkram
+ */
+const IcosahedronShaderMaterial = shaderMaterial(
+    //uniforms
+    {
+        uTime :0, 
+        uColor : new THREE.Color(1.0, 0.0, 0.0),
+        uTexture : new THREE.Texture(),
+    },
+    //Vertex shader
+    glsl`
+    varying vec2 vUv;
+    varying vec3 vNormal;
+    float PI = 3.141592653589793238;
+    void main() {
+        vUv = uv;
+        vNormal = normalize(normalMatrix*normal);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+    `,
+    //Fragment shader
+    glsl`
+    precision highp float;
+    varying vec2 vUv;
+    varying vec3 vNormal;
+    float PI = 3.141592653589793238;
 
-// );
+    uniform vec3 uColor;
+    uniform float uTime;
+    uniform sampler2D uTexture;
 
+    void main(){
+        vec3 X = dFdx(vNormal);
+        vec3 Y = dFdy(vNormal);
+        vec3 normal = normalize(cross(X,Y));
+
+        float diffuse = dot(normal, vec3(1.0, 1.0, 1.0))*0.5;
+
+        vec4 texture = texture2D(uTexture, vUv);
+        gl_FragColor = vec4(diffuse * texture);
+    }
+    `,
+);
+extend({IcosahedronShaderMaterial});
+
+/**
+ * Defines the Icosahedron mesh with its custom shaders & texture loaders
+ * @param {HTML props} props 
+ * @returns {JSX} the Icosahedron mesh in JSX format
+ */
 function Icosahedron(props){
     const mesh = useRef();
+    const matRef = useRef();
     // const [colorMap] = useLoader(TextureLoader, ['astronaut.jpg']);
 
     const [
         colorMap,
-        displacementMap,
-        normalMap,
-        roughnessMap,
-        aoMap
       ] = useLoader(TextureLoader, [
-        "satelite.jpg",
-        "satelite.jpg",
-        "satelite.jpg",
-        "satelite.jpg",
         "satelite.jpg",
       ]);
     
     useFrame(({ clock }) => {
-        // const a = clock.getElapsedTime();
-        // mesh.current.rotation.x = a;
-        // mesh.current.rotation.y = a;
-        // mesh.current.rotation.z = a;
+        matRef.current.uTime = clock.getElapsedTime();
     });
 
     return(
@@ -41,14 +80,19 @@ function Icosahedron(props){
                     <icosahedronGeometry 
                         args = {[1,1]} 
                     />
-                    <meshStandardMaterial
+                    <icosahedronShaderMaterial 
+                        ref = {matRef}
+                        uTexture = {colorMap}
+                        uColor = {"hotpink"}
+                    />
+                    {/* <meshStandardMaterial
                         displacementScale={0.0}
                         map={colorMap}
                         displacementMap={displacementMap}
                         normalMap={normalMap}
                         roughnessMap={roughnessMap}
                         aoMap={aoMap}
-                    />
+                    /> */}
             </mesh>
     );
 }
@@ -59,11 +103,11 @@ function ShaderIcosahedron(props){
         <div id="canvas-container" style = {{height: "100vh", width : "100vw"}}>
             <Canvas>
                 <Suspense fallback = {null}>
-                    <OrbitControls autoRotate />
+                    <OrbitControls />
                     <Icosahedron/>
                 </Suspense>
-                <ambientLight intensity = {0.2}/>
-                <directionalLight/>
+                <ambientLight/>
+                {/* <directionalLight/> */}
             </Canvas>
         </div>
     )

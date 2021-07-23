ed67aa69dc7e4e0f49d264ae65158a387254a79d
alexandreLamarre <alex7285@gmail.com>
2021-07-22 21:21:04 -0400

    refraction shader

--- src/components/3d/shaderIcosahedron/shaderIcosahedron.js
+++ src/components/3d/shaderIcosahedron/shaderIcosahedron.js
@@ -21,10 +21,16 @@ const IcosahedronShaderMaterial = shaderMaterial(
     glsl`
     varying vec2 vUv;
     varying vec3 vNormal;
+    varying vec3 eyeVector;
     float PI = 3.141592653589793238;
+
     void main() {
         vUv = uv;
         vNormal = normalize(normalMatrix*normal);
+
+        vec3 newPosition = position;
+        vec4 worldPosition = modelMatrix * vec4(newPosition, 1.0);
+        eyeVector = normalize(worldPosition.xyz - cameraPosition);
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
     `,
@@ -33,21 +39,42 @@ const IcosahedronShaderMaterial = shaderMaterial(
     precision highp float;
     varying vec2 vUv;
     varying vec3 vNormal;
+    varying vec3 eyeVector;
     float PI = 3.141592653589793238;
 
     uniform vec3 uColor;
     uniform float uTime;
     uniform sampler2D uTexture;
 
+    //hash function
+    vec2 hash22(vec2 p){
+        p = fract(p * vec2(5.3983, 5.4427));
+        p += dot(p.yx, p.xy + vec2(21.5351, 14.3137));
+        return fract(vec2(p.x * p.y * 95.4337, p.x * p.y * 97.597));
+    }
+
     void main(){
+        
         vec3 X = dFdx(vNormal);
         vec3 Y = dFdy(vNormal);
         vec3 normal = normalize(cross(X,Y));
+        float diffuse = dot(normal, vec3(1.0, 1.0, 1.0));
 
-        float diffuse = dot(normal, vec3(1.0, 1.0, 1.0))*0.5;
+        vec2 rand = hash22(vec2(floor(diffuse*20.0)));
 
-        vec4 texture = texture2D(uTexture, vUv);
-        gl_FragColor = vec4(diffuse * texture);
+        vec3 refracted = refract(eyeVector, normal, 1.0/3.0);
+        vec2 uvv = vec2(
+            sign(rand.x - 0.5)*1.0 + (rand.x -0.5) * 0.06,
+            sign(rand.y - 0.5)*1.0 + (rand.y-0.5) * 0.06
+        );
+
+        vec2 uv = rand * gl_FragCoord.xy/vec2(1000); //TODO:change 1000 to aspect ratio
+        uv += 1.*refracted.xy;
+
+        
+
+        vec4 texture = texture2D(uTexture, uv);
+        gl_FragColor = vec4(texture);//diffuse * texture);
     }
     `,
 );
@@ -61,7 +88,6 @@ extend({IcosahedronShaderMaterial});
 function Icosahedron(props){
     const mesh = useRef();
     const matRef = useRef();
-    // const [colorMap] = useLoader(TextureLoader, ['astronaut.jpg']);
 
     const [
         colorMap,
@@ -70,6 +96,8 @@ function Icosahedron(props){
       ]);
     
     useFrame(({ clock }) => {
+        mesh.current.rotation.x = clock.getElapsedTime()*0.03;
+        mesh.current.rotation.y = clock.getElapsedTime()*0.03;
         matRef.current.uTime = clock.getElapsedTime();
     });
 
@@ -101,7 +129,7 @@ function ShaderIcosahedron(props){
 
     return (
         <div id="canvas-container" style = {{height: "100vh", width : "100vw"}}>
-            <Canvas>
+            <Canvas onCreated={state => state.gl.setClearColor("#313639")}>
                 <Suspense fallback = {null}>
                     <OrbitControls />
                     <Icosahedron/>

71a13ef823eaee02b3e3ef7e009308d32fbc9315
alexandreLamarre <alex7285@gmail.com>
2021-10-22 18:48:24 -0400

    add tailwind, move old components into legacy folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import ShaderIcosahedron from './components/3d/shaderIcosahedron';
+//import ShaderIcosahedron from './components/3d/shaderIcosahedron';
 // import RegularPolytope from "./components/3d/torus";
-// import MainTitle from "./components/titles";
+//import MainTitle from "./components/titles";
 // import SceneWrapper from './components/3d/sceneWrapper';
 function App() {
   return (
     <div className="App">
-      {/* <MainTitle/>
+    {/*} <MainTitle/> {/** 
       <RegularPolytope/> */}
       {/* <SceneWrapper/> */}
-      <ShaderIcosahedron/>
     </div>
   );
 }

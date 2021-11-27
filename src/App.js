523964b2c3ad10c803ac952d8c7ecfd008e49238
alexandreLamarre <alex7285@gmail.com>
2021-11-27 14:55:52 -0500

    added grid to default portfolio state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import TopNavigation from './components/navigation';
 import SiderBar from './components/sidebar';
+import GridScene from './components/three/gridNoise';
 //import ThreeScene from './components/three/threescene';
 // import LavaScene from './components/three/lava/lavaComponent';
 //import ShaderIcosahedron from './components/3d/shaderIcosahedron';
 // import RegularPolytope from "./components/3d/torus";
 //import MainTitle from "./components/titles";
 // import SceneWrapper from './components/3d/sceneWrapper';
-import ParticleText from './components/three/particleText';
+// import ParticleText from './components/three/particleText';
 
 function App() {
   return (
     <div className="App">
-      <ParticleText/>
+      <GridScene></GridScene>
       <SiderBar/>
       <TopNavigation/>
     {/*} <MainTitle/> {/** 

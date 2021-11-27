8290fdbe77a62d70b33d56725f67821d9d98cc42
alexandreLamarre <alex7285@gmail.com>
2021-11-27 15:00:10 -0500

    tweak parameter

--- src/components/three/gridNoise/gridScene.js
+++ src/components/three/gridNoise/gridScene.js
@@ -49,7 +49,7 @@ export default function GridScene() {
                     grid={{
                         width: 100,
                         height: 100,
-                        sep: 0.2
+                        sep: 0.4
                     }}
                     zOfXYT={zOfXYT}
                     colorOfXYZT={colorOfXYZT}

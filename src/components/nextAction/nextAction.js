e4baf139f0c14e035c15bac9efd2a2cf555ad525
alexandreLamarre <alex7285@gmail.com>
2021-11-29 17:05:45 -0500

    linter update

--- src/components/nextAction/nextAction.js
+++ src/components/nextAction/nextAction.js
@@ -1,25 +1,25 @@
 /**
- * Text bubble that specifies next action input 
+ * Text bubble that specifies next action input
  * from user in order to transition further into the portfolio.
- * 
+ *
  * Specifies which action to take to close overlays.
- * 
+ *
  * Not visible during transition animations.
- * 
+ *
  * TODO: Add a timeout timer every X seconds to bring attention to the bubble.
- * 
- * @param {*} props 
- * @returns 
+ *
+ * @param {*} props
+ * @returns
  */
 const NextAction = (props) => {
-    return (
-        <div className='action-container'>
-            <span className="next-action">
-                Scroll down to continue 
-            </span>
-        </div>
-        
-    )
+  return (
+    <div className='action-container'>
+      <span className='next-action'>
+        Scroll down to continue
+      </span>
+    </div>
+
+  )
 }
 
 export default NextAction;

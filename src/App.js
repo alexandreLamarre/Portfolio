5b3519dfc5a3721f7d407790c839c9bb75c44880
alexandreLamarre <alex7285@gmail.com>
2021-11-30 19:12:15 -0500

    NOT-ASYNC-SAFE  redux store change onkeydown

--- src/App.js
+++ src/App.js
@@ -10,10 +10,36 @@ import NextAction from './components/nextAction'
 // import MainTitle from "./components/titles";
 // import SceneWrapper from './components/3d/sceneWrapper';
 // import ParticleText from './components/three/particleText';
+import {useDispatch} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './action';
+import useEventListener from './hooks/useEventListener';
+
+const NEXT_KEY = ['ArrowDown'];
+const PREV_KEY = ['ArrowUp'];
 
 function App () {
+
+  const dispatch = useDispatch();
+  const {transitionNext, transitionPrev} = bindActionCreators(actionCreators, dispatch);
+
+  function handleNextKey({key}) {
+    console.log(key);
+    if (NEXT_KEY.includes(String(key))){
+      //transition redux state to next page
+      console.log('transition next');
+      transitionNext();
+    }
+    else if (PREV_KEY.includes(String(key))) {
+      //transition redux state to prev page
+      transitionPrev();
+    }
+  }
+
+  useEventListener('keydown', handleNextKey);
+
   return (
-    <div className='App'>
+    <div className='App' onKeyDown={(e) => console.log(e) }>
       <GridScene />
       <SiderBar />
       <TopNavigation />

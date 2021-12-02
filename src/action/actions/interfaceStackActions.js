3527a3b5a27cd4f70c1c83fcd77eba33826708f7
alexandreLamarre <alex7285@gmail.com>
2021-12-02 11:26:26 -0500

    interface stack as redux actions

---
+++ src/action/actions/interfaceStackActions.js
@@ -0,0 +1,38 @@
+
+/**
+ * Adds the interface with the given number to the redux store interface stack
+ * @param {Number} number 
+ * @returns 
+ */
+export const addInterfaceStack = (number) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'add-interface',
+            payload: number,
+        })
+    };
+};
+
+/**
+ * Removes the interface with the given number to the redux store interface stack
+ * @param {Number} number 
+ * @returns 
+ */
+export const removeInterfaceStack = (number) => {
+    return (dispatch) => {
+            dispatch({
+                type : 'remove-interface',
+                payload : number,
+            })
+    };
+};
+
+export const removeTopInterface = (number) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'remove-top',
+            payload : undefined,
+        })
+    };
+};
+
